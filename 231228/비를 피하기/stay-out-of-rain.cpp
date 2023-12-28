ba7e05aa834c21f6b98281c316e7a076f3ce3303
임준범 <104415061+beomjunlim@users.noreply.github.com>
2023-12-28 21:54:55 +0900

    Title: 비를 피하기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 231228/비를 피하기/stay-out-of-rain.cpp
+++ 231228/비를 피하기/stay-out-of-rain.cpp
@@ -44,8 +44,11 @@ int main() {
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             cin>>arr[i][j];
-            if(arr[i][j]==3)
+            if(arr[i][j]==3){
                 q.push(make_pair(i,j));
+                visited[i][j] = true;
+            }
+
         }
     }
     
