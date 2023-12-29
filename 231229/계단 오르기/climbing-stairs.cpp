f7644836a766bda35187d21ec8e75cd5d6ddfd64
임준범 <104415061+beomjunlim@users.noreply.github.com>
2023-12-29 19:43:06 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231229/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 1001
+
+int arr[MAX];
+
+int main() {
+    int n;
+    cin>>n;
+
+    arr[1] = 0;
+    arr[2] = 1;
+    arr[3] = 1;
+
+    for(int i=4; i<=n; i++){
+        arr[i] = (arr[i-2]+arr[i-3]);
+    }
+    cout<<arr[n];
+    return 0;
+}
