77d85b6dd86b743e7caa2d224bd5b15a85107162
임준범 <104415061+beomjunlim@users.noreply.github.com>
2023-12-30 22:58:40 +0900

    Title: 알바로 부자 되기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231230/알바로 부자 되기/being-rich-by-working-part-time.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+#define MAX 1000
+
+int dp[MAX];
+
+bool compare(pair<int,int> a, pair<int,int> b){
+    return a.second<b.second;
+}
+
+int main() {
+    vector<pair<int,int>> v;
+    vector<pair<int,int>> w;
+    int n;
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        v.push_back(make_pair(a,b));
+        w.push_back(make_pair(b,c));
+    }
+
+    sort(v.begin(), v.end(), compare);
+    sort(w.begin(), w.end());
+
+    for(int i=0; i<n; i++){
+        dp[i] = w[i].second;
+        for(int j=0; j<i; j++){
+            if(v[i].first>v[j].second)
+                dp[i] = max(dp[i], dp[j] + w[j].second);
+        }
+    }
+
+    sort(dp, dp+n);
+    cout<<dp[n-1];
+
+    return 0;
+}
