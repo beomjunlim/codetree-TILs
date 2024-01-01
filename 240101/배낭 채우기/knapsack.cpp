25896e60c977bcaabe0dbc19072cb0a77e060488
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-01-01 21:18:39 +0900

    Title: 배낭 채우기, Time: 9ms, Memory: 4MB, Status: Passed - CodeTree

---
+++ 240101/배낭 채우기/knapsack.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX_N 100
+#define MAX_M 10000
+
+int dp[MAX_N+1][MAX_M+1];
+int weight[MAX_N+1];
+int value[MAX_N+1];
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+
+    for(int i=1; i<=n; i++){
+        cin>>weight[i];
+        cin>>value[i];
+    }
+
+    for(int i=1; i<=n; i++){
+        for(int j=0; j<=m; j++){
+            if(j>=weight[i]){
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i]] + value[i]);
+            }
+            else{
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+
+    cout<<dp[n][m];
+    return 0;
+}
