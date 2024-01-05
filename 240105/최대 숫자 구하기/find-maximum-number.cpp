e4502fc09bd3a3159c5c4371c6a24acada820755
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-01-05 16:19:09 +0900

    Title: 최대 숫자 구하기, Time: 123ms, Memory: 4MB, Status: Passed - CodeTree

---
+++ 240105/최대 숫자 구하기/find-maximum-number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+
+int main() {
+    set<int> s;
+    int n,m;
+    cin>>n>>m;
+
+    for(int i=1; i<=m; i++){
+        s.insert(i);
+    }
+
+    for(int i=0; i<n; i++){
+        int a;
+        cin>>a;
+        s.erase(a);
+        cout<<*s.rbegin()<<'\n';
+    }
+    return 0;
+}
