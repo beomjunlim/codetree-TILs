582406eaa70a68d88b7e26b8f1256b6db74d122a
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-01-07 12:39:38 +0900

    Title: 최솟값 3개, Time: 184ms, Memory: 1MB, Status: Passed - CodeTree

--- 240107/최솟값 3개/top-3-smallest-number.cpp
+++ 240107/최솟값 3개/top-3-smallest-number.cpp
@@ -5,24 +5,27 @@ using namespace std;
 int main() {
     int n;
     cin>>n;
-    priority_queue<int> pq;
+    priority_queue<pair<int,int>> pq;
 
     for(int i=0; i<n; i++){
         int x;
         cin>>x;
-        pq.push(-x);
+        pq.push(make_pair(-x,i));
         if(pq.size()<3)
             cout<<-1<<'\n';
         else{
-            int a,b,c;
-            a = pq.top();
+            int a,b;
+            int index_a,index_b;
+            a = pq.top().first;
+            index_a = pq.top().second;
             pq.pop();
-            b = pq.top();
+            b = pq.top().first;
+            index_b = pq.top().second;
             pq.pop();
-            c = pq.top();
-            pq.push(a);
-            pq.push(b);
-            cout<<-a*b*c<<'\n';
+            long long sum = (long long)a*b*(-pq.top().first);
+            cout<<sum<<'\n';
+            pq.push(make_pair(a, index_a));
+            pq.push(make_pair(b, index_b));
         }
     }
     return 0;
