01009b2fbc19fa634b8e49cad3ad70f681c41a55
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-01-07 18:55:36 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240107/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 100000
+int arr[MAX+1];
+int num[MAX+1];
+
+int main() {
+    int n,q;
+    cin>>n>>q;
+
+    int max_num = 0;
+    for(int i=0; i<n; i++){
+        int x;
+        cin>>x;
+        arr[x] = 1;
+        max_num = max(max_num, x);
+    }
+
+    for(int i=1; i<=max_num; i++){
+        num[i] = num[i-1] + arr[i];
+    }
+
+    while(q>0){
+        int x,y;
+        cin>>x>>y;
+        if(x>max_num)
+            num[x] = num[y] = num[max_num];
+        
+        else if(y>max_num)
+            num[y] = num[max_num];
+            
+        cout<<num[y]-num[x-1]<<'\n';
+        q--;
+    }
+
+
+    return 0;
+}
