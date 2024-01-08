4086052794987125e14a3fe63f771c381cb27e60
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-01-08 16:55:50 +0900

    Title: 점 개수 세기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240108/점 개수 세기/count-number-of-points.cpp
+++ 240108/점 개수 세기/count-number-of-points.cpp
@@ -25,30 +25,30 @@ int main() {
     for(int i=0; i<q; i++){
         int x,y;
         cin>>x>>y;
-        int num_x;
-        int num_y;
-        if(num.lower_bound(x)==num.end()){
-            cout<<0<<'\n'; // 1
-            continue;
-        }
+        int num_x = mapper[*num.lower_bound(x)];
+        int num_y = mapper[*num.upper_bound(y)];
+        // if(num.lower_bound(x)==num.end()){
+        //     cout<<0<<'\n'; // 1
+        //     continue;
+        // }
         
-        if(num.lower_bound(y)!=num.find(y)&&num.lower_bound(y)==num.begin()){
-            cout<<0<<'\n'; // 2
-            continue;
-        }
+        // if(num.lower_bound(y)!=num.find(y)&&num.lower_bound(y)==num.begin()){
+        //     cout<<0<<'\n'; // 2
+        //     continue;
+        // }
 
-        else if(num.lower_bound(x)!=num.find(x)&&num.lower_bound(x)==num.begin())
-            num_x = 0;
-        else 
-            num_x = mapper[*prev(num.lower_bound(x))];
+        // else if(num.lower_bound(x)!=num.find(x)&&num.lower_bound(x)==num.begin())
+        //     num_x = 0;
+        // else 
+        //     num_x = mapper[*prev(num.lower_bound(x))];
 
 
-        if(num.lower_bound(y)==num.end())
-            num_y = mapper[*prev(num.end())];
-        else if(num.find(y)==num.lower_bound(y))
-            num_y = mapper[*num.lower_bound(y)];
-        else
-            num_y = mapper[*prev(num.lower_bound(y))];
+        // if(num.lower_bound(y)==num.end())
+        //     num_y = mapper[*prev(num.end())];
+        // else if(num.find(y)==num.lower_bound(y))
+        //     num_y = mapper[*num.lower_bound(y)];
+        // else
+        //     num_y = mapper[*prev(num.lower_bound(y))];
 
         cout<<num_y - num_x<<'\n';
     }
