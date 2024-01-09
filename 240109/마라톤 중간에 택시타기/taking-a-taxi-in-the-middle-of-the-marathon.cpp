0910c8ab31f5d329cce2d9f112a64faf1f33a349
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-01-09 17:05:38 +0900

    Title: 마라톤 중간에 택시타기, Time: 86ms, Memory: 1MB, Status: Passed - CodeTree

--- 240109/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.cpp
+++ 240109/마라톤 중간에 택시타기/taking-a-taxi-in-the-middle-of-the-marathon.cpp
@@ -21,10 +21,15 @@ int main() {
 
     for(int i=2; i<=n; i++){
         L[i] = L[i-1] + (abs(x[i]-x[i-1])+abs(y[i]-y[i-1]));
-        R[n-i+1] = R[n-i+2] + (abs(x[n-i+1]-x[n-i+2]+abs(y[n-i+1]-y[n-i+2])));
+  
+    }
+
+    for(int i=n-1; i>=0; i--){
+        R[i] = R[i+1] + (abs(x[i]-x[i+1])+abs(y[i]-y[i+1]));
     }
 
     int ans = INT_MAX;
+    
     for(int i=2; i<n; i++){
         ans = min(ans, L[i-1]+R[i+1]+abs(x[i-1]-x[i+1])+abs(y[i-1]-y[i+1]));
     }
