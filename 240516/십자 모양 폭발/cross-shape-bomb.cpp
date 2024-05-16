398ac57631880a4ec15b48477618a580d17d51fa
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-05-16 09:22:07 +0900

    Title: 십자 모양 폭발, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240516/십자 모양 폭발/cross-shape-bomb.cpp
+++ 240516/십자 모양 폭발/cross-shape-bomb.cpp
@@ -1,77 +1,101 @@
 #include <iostream>
 using namespace std;
 
-int n,r,c;
+int n, r, c;
 int arr[201][201];
+int next_arr[201][201]; // next_arr 추가
 
-void down(int y){
-    for(int i=r; i>1; i--){
-        arr[i][y] = arr[i-1][y];
+void down(int y) {
+    for (int i = n - 1; i > 0; i--) {
+        if (arr[i][y] == 0) {
+            for (int k = i; k > 0; k--) {
+                arr[k][y] = arr[k - 1][y];
+            }
+            arr[0][y] = 0;
+        }
     }
-    arr[1][y] = 0;
 }
 
-void row(){
+void row() {
     int num = arr[r][c];
-    int start = 1;
-    int end = n;
+    int start = 0;
+    int end = n - 1;
 
-    if(r-num+1>0)
+    if (r - num + 1 >= 0)
         start = r - num + 1;
-    
-    if(r+num-1<=n)
+
+    if (r + num - 1 < n)
         end = r + num - 1;
 
-    for(int i=start-1; i>=1; i--){
+    for (int i = start - 1; i >= 0; i--) {
         arr[end][c] = arr[i][c];
         end--;
     }
 
-    for(int i=num+1; i>=1; i--){
+    for (int i = r - num + 1; i <= r + num - 1; i++) {
         arr[i][c] = 0;
     }
 }
 
-void bomb(){
+void bomb() {
     int num = arr[r][c];
-    int start = 1;
-    int end = n;
+    int start = 0;
+    int end = n - 1;
 
-    if(c-num+1>=1)
-        start = c-num+1;
-    
-    if(c+num-1<=n)
-        end = c+num-1;
-    
-    for(int i=start; i<=end; i++){
-        if(i==c){
+    if (c - num + 1 >= 0)
+        start = c - num + 1;
+
+    if (c + num - 1 < n)
+        end = c + num - 1;
+
+    for (int i = start; i <= end; i++) {
+        if (i == c) {
             row();
-        }
-        else{
+        } else {
             down(i);
         }
     }
+
+    // 폭탄이 터진 이후의 결과를 next_arr에 저장합니다.
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            next_arr[i][j] = 0;
+
+    for (int j = 0; j < n; j++) {
+        int next_row = n - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i][j])
+                next_arr[next_row--][j] = arr[i][j];
+        }
+    }
+
+    // next_arr 값을 arr로 옮깁니다.
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            arr[i][j] = next_arr[i][j];
 }
 
 int main() {
-    cin>>n;
+    cin >> n;
 
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            cin>>arr[i][j];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
         }
     }
 
-    cin>>r>>c;
+    cin >> r >> c;
+    r--; // 1-based index를 0-based로 변경
+    c--;
+
     bomb();
-    
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<'\n';
-    }
 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << '\n';
+    }
 
     return 0;
 }
