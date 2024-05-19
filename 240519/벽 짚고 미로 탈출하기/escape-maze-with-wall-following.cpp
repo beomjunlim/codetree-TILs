0ff323435748eaef315ceca62ca17ee31ca60237
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-05-19 10:53:08 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240519/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.cpp
+++ 240519/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.cpp
@@ -15,7 +15,6 @@ bool InRange(int x, int y){
 }
 
 void simulation(int x, int y, int dir, int cnt){
-
     if(!InRange(x,y)){
         sw = true;
         ans = cnt;
@@ -29,31 +28,81 @@ void simulation(int x, int y, int dir, int cnt){
 
     visited[x][y][dir] = true;
 
-    int check_x = x + dx[(dir+1)%4];
-    int check_y = y + dy[(dir+1)%4];
+    int right_x = x + dx[(dir+1)%4];
+    int right_y = y + dy[(dir+1)%4];
+    int nx = x + dx[dir];
+    int ny = y + dy[dir];
 
-    if(InRange(check_x, check_y)&&arr[check_x][check_y]=='#'){
-        int nx = x + dx[dir];
-        int ny = y + dy[dir];
-        if(arr[nx][ny]=='#'){
-            dir = (dir+1)%4;
-            simulation(x,y, dir, cnt);
-        }
-        else{
+    if(InRange(right_x,right_y)&&arr[right_x][right_y]=='#'){
+        if(InRange(nx,ny)&&arr[nx][ny]=='#')
+            simulation(x,y, (dir+1)%4, cnt);
+        else
             simulation(nx,ny, dir, cnt+1);
-        }
     }
 
     else{
         dir = (dir+1)%4;
-        int nx = x + dx[dir];
-        int ny = y + dy[dir];
+        nx = x + dx[dir];
+        ny = y + dy[dir];
+        if(InRange(nx,ny)&&arr[nx][ny]=='#')
+            simulation(x,y, dir, cnt);
+        else{
+            if(!visited[nx][ny][(dir+2)%4])
+                simulation(nx,ny,dir,cnt+1);
+            else
+                simulation(x,y, (dir+1)%4, cnt);
+        }
 
-        if(InRange(nx,ny)&&arr[nx][ny]=='.')
-            simulation(nx,ny, dir, cnt+1);
-        else  
-            simulation(x, y, dir, cnt);
     }
+
+    // int nx = x + dx[dir];
+    // int ny = y + dy[dir];
+
+    // if(InRange(nx,ny)&&arr[nx][ny]=='#'){
+    //     simulation(x,y, (dir+1)%4, cnt);
+    // }
+    // else{
+    //     int right_x = x + dx[(dir+1)%4];
+    //     int right_y = y + dy[(dir+1)%4];
+
+    //     if(InRange(right_x,right_y)&&arr[right_x][right_y]=='#'){
+    //         simulation(nx,ny, dir, cnt+1);
+    //     }
+    //     else{
+    //         dir = (dir+1)%4;
+    //         nx = x + dx[dir];
+    //         ny = y + dy[dir];
+    //         simulation(nx,ny,dir,cnt+1);
+    //     }
+    // }
+
+    // int check_x = x + dx[(dir+1)%4];
+    // int check_y = y + dy[(dir+1)%4];
+
+    // if(InRange(check_x, check_y)&&arr[check_x][check_y]=='#'){
+    //     int nx = x + dx[dir];
+    //     int ny = y + dy[dir];
+    //     if(arr[nx][ny]=='#'){
+    //         dir = (dir+1)%4;
+    //         simulation(x,y, dir, cnt);
+    //     }
+    //     else{
+    //         simulation(nx,ny, dir, cnt+1);
+    //     }
+    // }
+
+    // else{
+    //     int nx = x + dx[dir];
+    //     int ny = y + dy[dir];
+    //     if(InRange(nx,ny)&&arr[nx][ny]=='#'){
+    //         dir = (dir+1)%4;
+    //         simulation(x,y, dir, cnt);
+    //     }
+    //     else{
+    //         dir = (dir+1)%4;
+    //         simulation(nx,ny, dir, cnt+1);
+    //     }
+    // }
 }
 
 int main() {
