fed306bd627513b7adf701fdca4ef1d5d3a12594
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-05-19 09:25:20 +0900

    Title: 떨어지는 1자 블록, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240519/떨어지는 1자 블록/falling-horizontal-block.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int n,m,k;
+int arr[100][100];
+
+int main() {
+    cin>>n>>k>>m;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin>>arr[i][j];
+        }
+    }
+    
+    bool sw;
+    m = m-1;
+
+    for(int i=0; i<n-1; i++){
+        sw = false;
+        for(int j=m; j<m+k; j++){
+            if(arr[i+1][j]==1){
+                sw = true;
+            }
+        }
+
+        if(sw){
+            for(int j=m; j<m+k; j++){
+                arr[i][j] = 1;
+            }
+            break;
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<'\n';
+    }
+    return 0;
+}
