fcf2ec40156704ac14c8d797a7de8eb4f96ff906
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-06-24 08:54:13 +0900

    Title: 원점으로부터의 거리, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240624/원점으로부터의 거리/distance-from-origin.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <utility>
+using namespace std;
+
+pair<int, pair<int,int>> num[1001];
+
+bool cmp(pair<int, pair<int,int>> a, pair<int, pair<int,int>> b){
+    return abs(a.second.first) + abs(a.second.second) < abs(b.second.first) + abs(b.second.second);
+}
+
+int main() {
+    int n;
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        int a,b;
+        cin>>a>>b;
+        num[i] = make_pair(i+1,make_pair(a,b));
+    }
+
+    sort(num, num+n, cmp);
+
+    for(int i=0; i<n; i++){
+        cout<<num[i].first<<'\n';
+    }
+
+    return 0;
+}
