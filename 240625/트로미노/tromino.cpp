02ee59509603467da2b93ebfecf494b81dedcd86
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-06-25 07:54:39 +0900

    Title: 트로미노, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240625/트로미노/tromino.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n,m;
+int ans = 0;
+int arr[201][201];
+
+int main() {
+    cin>>n>>m;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m-2; j++){
+            ans = max(ans, arr[i][j] + arr[i][j+1] + arr[i][j+2]);
+            ans = max(ans, arr[j][i] + arr[j+1][i] + arr[j+2][i]);
+        }
+    }
+
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<m-1; j++){
+            int temp = arr[i][j] + arr[i+1][j] + arr[i][j+1] + arr[i+1][j+1];
+            ans = max(ans, max(temp - arr[i][j], max(temp - arr[i+1][j], max(temp - arr[i][j+1], temp - arr[i+1][j+1]))));
+        }
+    }
+
+    cout<<ans;
+    return 0;
+}
