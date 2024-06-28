ce7692b95c12e965268a20438ec9cdf9de7988f4
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-06-28 11:40:50 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 1055ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240628/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int n,m;
+bool sw = false;
+int arr[101][101];
+int dx[] = {1,0};
+int dy[] = {0,1};
+
+bool InRange(int x, int y){
+    return 0<=x&&x<n&&0<=y&&y<m;
+}
+
+void dfs(int x, int y){
+    if(x==n-1&&y==m-1){
+        sw = true;
+        return;
+    }
+
+    for(int i=0; i<2; i++){
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if(InRange(nx,ny)&&arr[nx][ny]==1){
+            dfs(nx,ny);
+        }
+    }
+}
+
+int main() {
+    cin>>n>>m;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    dfs(0,0);
+    cout<<sw<<'\n';
+    return 0;
+}
