c0ffbcfe1f9095bb7811f2dced38ec5de90f22ae
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-05 12:38:57 +0900

    Title: 일렬로 서있는 소 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240705/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+
+    int arr[101];
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int ans = 0;
+    for(int i=0; i<n-2; i++){
+        for(int j=i+1; j<n-1; j++){
+            if(arr[i]<arr[j]){
+                for(int k =j+1; k<n; k++){
+                    if(arr[j]<arr[k])
+                        ans++;
+                }
+            }
+        }
+    }
+
+    cout<<ans;
+    return 0;
+}
