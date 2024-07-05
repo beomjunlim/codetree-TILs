023ad8e8a6197184237a0dbe0206de8f51212931
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-05 12:39:19 +0900

    Title: 일렬로 서있는 소 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240705/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
+++ 240705/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
@@ -14,9 +14,9 @@ int main() {
     int ans = 0;
     for(int i=0; i<n-2; i++){
         for(int j=i+1; j<n-1; j++){
-            if(arr[i]<arr[j]){
+            if(arr[i]<=arr[j]){
                 for(int k =j+1; k<n; k++){
-                    if(arr[j]<arr[k])
+                    if(arr[j]<=arr[k])
                         ans++;
                 }
             }
