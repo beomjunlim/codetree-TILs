a7f4321fa8b342f4dd1fbc3008994d2ff584b474
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-06 14:25:29 +0900

    Title: 구간 중 최대 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240706/구간 중 최대 합/max-sum-of-subarray.cpp
+++ 240706/구간 중 최대 합/max-sum-of-subarray.cpp
@@ -13,7 +13,7 @@ int main() {
 
     int ans = 0;
 
-    for(int i=0; i<n-k; i++){
+    for(int i=0; i<=n-k; i++){
         int sum = 0;
         for(int j=i; j<i+k; j++){
             sum += arr[j];
