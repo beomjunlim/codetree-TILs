758b841d941032b5e6f3b8060f04398c9639adaa
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-07 10:26:03 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240707/가장 가까운 두 점 사이의 거리/closest-pair-of-points.cpp
+++ 240707/가장 가까운 두 점 사이의 거리/closest-pair-of-points.cpp
@@ -1,32 +1,13 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
-#include <vector>
 using namespace std;
 
 int n;
 long long ans = 9876543210000;
 int X[101];
 int Y[101];
-vector<pair<int,int>> v;
 
-void backtracking(int num, int cnt){
-    if(cnt==2){
-        int x1 = v[0].first;
-        int y1 = v[0].second;
-        int x2 = v[1].first;
-        int y2 = v[1].second;
-
-        long long sum = pow(abs(x1-x2),2) + pow(abs(y1-y2),2);
-        ans = min(ans, sum);
-    }
-
-    for(int i=num; i<n; i++){
-        v.push_back(make_pair(X[i],Y[i]));
-        backtracking(i+1, cnt+1);
-        v.pop_back();
-    }
-}
 int main() {
     cin>>n;
 
@@ -34,7 +15,14 @@ int main() {
         cin>>X[i]>>Y[i];
     }
 
-    backtracking(0,0);
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            long long dx = X[i] - X[j];
+            long long dy = Y[i] - Y[j];
+            long long dist = dx * dx + dy * dy;
+            ans = min(ans, dist);
+        }
+    }
     cout<<ans;
     return 0;
 }
