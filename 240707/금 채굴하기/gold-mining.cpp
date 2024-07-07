6c46c949d6bc0e774884a1d47394c5759ac23b9c
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-07 09:20:08 +0900

    Title: 금 채굴하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240707/금 채굴하기/gold-mining.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n,m;
+int ans = 0;
+int arr[21][21];
+
+void check(int low, int col, int size){
+    int sum = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            int a = abs(low-i);
+            int b = abs(col-j);
+
+            if(a+b<=size){
+                if(arr[i][j]==1)
+                    sum++;
+            }
+        }
+    }
+    int cost = (size*size + (size+1)*(size+1));
+
+    if(sum*m>=cost)
+        ans = max(ans,sum);
+}
+
+int main() {
+    cin>>n>>m;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int k=1; k<=2*(n-1); k++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                check(i,j,k);
+            }
+        }
+    }
+
+    cout<<ans;
+    return 0;
+}
