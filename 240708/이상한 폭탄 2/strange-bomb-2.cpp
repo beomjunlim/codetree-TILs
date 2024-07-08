a3ec23460505a6699f686663be8d51f5b2a76afd
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-08 14:46:54 +0900

    Title: 이상한 폭탄 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240708/이상한 폭탄 2/strange-bomb-2.cpp
+++ 240708/이상한 폭탄 2/strange-bomb-2.cpp
@@ -19,7 +19,7 @@ int main() {
 
         for(int j=0; j<n; j++){
             if(i!=j){
-                if(bomb==bombs[j]&&abs(i-j)<=3){
+                if(bomb==bombs[j]&&abs(i-j)<=k){
                     ans = max(ans,bomb);
                 }
             }
