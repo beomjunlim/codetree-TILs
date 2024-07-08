d64bedda0f13831c89da1adedc3eadf2660da9d4
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-08 15:16:54 +0900

    Title: 스승의 은혜 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240708/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n,m;
+vector<pair<int,int>> v;
+
+bool cmp(pair<int,int> a, pair<int,int> b){
+    return a.first + a.second < b.first + b.second; 
+}
+
+int main() {
+    cin>>n>>m;
+
+    for(int i=0; i<n; i++){
+        int x,y;
+        cin>>x>>y;
+        v.push_back(make_pair(x,y));
+    }
+
+    sort(v.begin(), v.end(), cmp);
+    int ans = 0;
+
+    for(int i=0; i<n; i++){
+        int sum = m;
+        sum -= v[i].first/2;
+        sum -= v[i].second;
+        int cnt = 1;
+        for(int j=0; j<n; j++){
+            if(i!=j){
+                int num = v[j].first + v[i].second;
+                if(sum - num >=0){
+                    sum -= num;
+                    cnt++;
+                }
+            }
+        }
+        ans = max(ans, cnt);
+    }
+    cout<<ans;
+    return 0;
+}
