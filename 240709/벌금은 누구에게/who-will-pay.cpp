949ab45e5e7d5342425160b792ea44487a442df7
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-09 16:47:36 +0900

    Title: 벌금은 누구에게, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240709/벌금은 누구에게/who-will-pay.cpp
+++ 240709/벌금은 누구에게/who-will-pay.cpp
@@ -4,19 +4,23 @@ using namespace std;
 int main() {
     int n,m,k;
     cin>>n>>m>>k;
-    int arr[101];
+    int arr[101] = {0};
 
     int ans = -1;
+    int idx = 0;
     bool sw = false;
     for(int i=0; i<m; i++){
         int x;
         cin>>x;
+        idx = x;
         arr[x]++;
         if(arr[x]>=k&&!sw){
             sw = true;
             ans = x;
         }
     }
+
+
 
     cout<<ans;
     return 0;
