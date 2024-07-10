5ab0f69da0875409444c9a33b4d84a64d078312f
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-10 19:46:42 +0900

    Title: 가장 먼저 나오는 숫자, Time: 265ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240710/가장 먼저 나오는 숫자/first-appear-number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+
+    long long arr[100001];
+
+    for(int i=0; i<n; i++)
+        cin>>arr[i];
+
+    for(int i=0; i<m; i++){
+        int x;
+        cin>>x;
+        int min_idx = lower_bound(arr, arr+n, x) - arr;
+        int max_idx = upper_bound(arr, arr+n, x) - arr;
+        if(max_idx-min_idx==0)
+            cout<<-1<<'\n';
+        else
+            cout<<min_idx+1<<'\n';
+    }
+    return 0;
+}
