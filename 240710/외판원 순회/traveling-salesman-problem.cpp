9ddcec7e9680dbb7fda67588f51e5bd2f75a8c88
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-10 16:57:38 +0900

    Title: 외판원 순회, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240710/외판원 순회/traveling-salesman-problem.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int ans = 987654321;
+int arr[11][11];
+bool visited[11];
+int low[11];
+
+void backtracking(int cnt){
+    if(cnt==n){
+        low[0] = 0;
+        int sum = 0;
+        int idx = low[0];
+        for(int i=1; i<n; i++){
+            sum += arr[idx][low[i]];
+            idx = low[i];
+        }
+        sum += arr[idx][0];
+        ans = min(ans, sum);
+        return;
+    }
+
+    for(int i=1; i<n; i++){
+        if(!visited[i]){
+            visited[i] = true;
+            low[cnt] = i;
+            backtracking(cnt+1);
+            visited[i] = false;
+        }
+    }
+}
+int main() {
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin>>arr[i][j];
+        }
+    }    
+
+    backtracking(1);
+    cout<<ans;
+    return 0;
+}
