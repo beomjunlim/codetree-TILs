d18da56c7708d197011de26cd0a5f70fd4f343a5
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-15 20:47:46 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240715/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.cpp
+++ 240715/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.cpp
@@ -2,57 +2,55 @@
 #include <algorithm>
 using namespace std;
 
-int n,m;
-int arr[101][101]={0};
+int n, m;
+int arr[101][101] = {0};
 int dist[101];
 bool visited[101];
 
-void dijkstra(int x){
-    for(int i=2; i<=n; i++){
-        if(x!=i&&arr[x][i]!=0){
-            dist[i] = min(dist[i], arr[x][i] + dist[x]);
-        }
-    }
-}
-int main() {
-    cin>>n>>m;
+void dijkstra(int start) {
+    fill(dist, dist + 101, 987654321); // dist 배열 초기화
+    dist[start] = 0;
 
-    for(int i=0; i<m; i++){
-        int x,y,dis;
-        cin>>x>>y>>dis;
-        arr[x][y] = dis;
-    }
-
-
-    dist[1] = 0;
-    
-    for(int i=2; i<=n; i++){
-        if(arr[1][i]!=0){
-            dist[i] = arr[1][i];
-        }
-        else
-            dist[i] = 987654321;
-    }
-    
-    for(int i=1; i<n; i++){
+    for (int i = 1; i <= n; i++) {
         int x = 987654321;
-        int idx;
-        for(int j=2; j<=n; j++){
-            if(!visited[j]&&x>dist[j]){
-                visited[j] = true;
+        int idx = -1;
+
+        for (int j = 1; j <= n; j++) {
+            if (!visited[j] && dist[j] < x) {
+                x = dist[j];
                 idx = j;
-                x= dist[j];
             }
         }
 
-        dijkstra(idx);
+        if (idx == -1) break;
+
+        visited[idx] = true;
+
+        for (int j = 1; j <= n; j++) {
+            if (!visited[j] && arr[idx][j] != 0) {
+                dist[j] = min(dist[j], dist[idx] + arr[idx][j]);
+            }
+        }
     }
-    
-    for(int i=2; i<=n; i++){
-        if(dist[i]==987654321)
-            cout<<-1<<'\n';
+}
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        int x, y, dis;
+        cin >> x >> y >> dis;
+        arr[x][y] = dis;
+    }
+
+    dijkstra(1);
+
+    for (int i = 2; i <= n; i++) {
+        if (dist[i] == 987654321)
+            cout << -1 << '\n';
         else
-            cout<<dist[i]<<'\n';
+            cout << dist[i] << '\n';
     }
+
     return 0;
 }
