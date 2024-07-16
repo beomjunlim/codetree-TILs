67eb63951fda3ef4385012fe9fb5893cf23a0d11
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-16 19:00:10 +0900

    Title: 가장 가까운 거리의 최댓값, Time: 217ms, Memory: 6MB, Status: Passed - CodeTree

--- 240716/가장 가까운 거리의 최댓값/maximum-of-nearest-distance.cpp
+++ 240716/가장 가까운 거리의 최댓값/maximum-of-nearest-distance.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <queue>
 using namespace std;
 
-int main() {
-    int n,m,a,b,c;
-    vector<pair<int,int>> arr[100001];
+int n,m,a,b,c;
+vector<pair<int,int>> arr[100001];
+int dist[100001][3];
 
+void dijkstra(int start, int cnt){
+    for(int i=1; i<=n; i++)
+        dist[i][cnt] = 987654321;
+    
+    dist[start][cnt] = 0;
+
+    priority_queue<pair<int,int>> pq;
+    pq.push(make_pair(0,start));
+
+    while(!pq.empty()){
+        int min_dist = -pq.top().first;
+        int min_idx = pq.top().second;
+        pq.pop();
+
+        if(dist[min_idx][cnt] < min_dist)
+            continue;
+
+        for(int i=0; i<arr[min_idx].size(); i++){
+            int next_idx = arr[min_idx][i].first;
+            int next_dist = arr[min_idx][i].second;
+
+            int new_dist = dist[min_idx][cnt] + next_dist;
+
+            if(dist[next_idx][cnt] > new_dist){
+                dist[next_idx][cnt] = new_dist;
+                pq.push(make_pair(-new_dist,next_idx));
+            }
+        }
+    }
+}
+
+int main() {
     cin>>n>>m;
     cin>>a>>b>>c;
 
@@ -17,49 +50,16 @@ int main() {
         arr[y].push_back(make_pair(x,dis));
     }
 
-    vector<int> node;
+    dijkstra(a,0);
+    dijkstra(b,1);
+    dijkstra(c,2);
 
-    for(int i=1; i<=n; i++){
-        if(i!=a&&i!=b&&i!=c)
-            node.push_back(i);
-    }
-
-    int dist[100001];
     int ans = 0;
-
-    for(int i=0; i<node.size(); i++){
-        int start = node[i];
-
-        for(int j=1; j<=n; j++)
-            dist[j] = 987654321;
-
-        dist[start] = 0;
-
-        priority_queue<pair<int,int>> pq;
-        pq.push(make_pair(0,start));
-
-        while(!pq.empty()){
-            int min_dist = -pq.top().first;
-            int min_idx = pq.top().second;
-            pq.pop();
-
-            if(dist[min_idx]<min_dist)
-                continue;
-            
-            for(int k=0; k<arr[min_idx].size(); k++){
-                int next_idx = arr[min_idx][k].first;
-                int next_dist = arr[min_idx][k].second;
-
-                int new_dist = dist[min_idx] + next_dist;
-                if(dist[next_idx]>new_dist){
-                    dist[next_idx] = new_dist;
-                    pq.push(make_pair(-new_dist, next_idx));
-                }
-            }
+    for(int i=1; i<=n; i++){
+        if(i!=a&&i!=b&&i!=c){
+            int cnt = min(dist[i][0], min(dist[i][1], dist[i][2]));
+            ans = max(ans, cnt);
         }
-
-        int cost = min(dist[a], min(dist[b], dist[c]));
-        ans = max(ans, cost);
     }
 
     cout<<ans;
