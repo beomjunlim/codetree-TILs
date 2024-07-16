365ac335078365d75277980cf99dacc1a31d5e77
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-16 20:18:20 +0900

    Title: 다른 괄호로 이동하기, Time: 242ms, Memory: 0MB, Status: Passed - CodeTree

--- 240716/다른 괄호로 이동하기/move-to-another-parenthesis.cpp
+++ 240716/다른 괄호로 이동하기/move-to-another-parenthesis.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
 #include <queue>
-
 using namespace std;
 
 int n,a,b;
 char arr[31][31];
 int dist[31][31];
-bool visited[31][31];
 int dx[] = {0,0,-1,1};
 int dy[] = {-1,1,0,0};
 int ans = 0;
 
+bool InRange(int x, int y){
+    return 0<=x&&x<n&&0<=y&&y<n;
+}
+
 void dijkstra(int x, int y){
-    memset(visited, false, sizeof(visited));
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             dist[i][j] = 987654321;
@@ -22,8 +22,6 @@ void dijkstra(int x, int y){
     }
 
     dist[x][y] = 0;
-    visited[x][y] = true;
-
     priority_queue<pair<int,pair<int,int>>> pq;
     pq.push(make_pair(0, make_pair(x,y)));
 
@@ -40,17 +38,17 @@ void dijkstra(int x, int y){
             int nx = min_x + dx[i];
             int ny = min_y + dy[i];
             int distance;
+            if(InRange(nx,ny)){
+                if(arr[min_x][min_y]==arr[nx][ny])
+                    distance = a;
+                else
+                    distance = b;
+                int new_distance = distance + dist[min_x][min_y];
 
-            if(arr[min_x][min_y]==arr[nx][ny])
-                distance = a;
-            else
-                distance = b;
-            int new_distance = distance + dist[min_x][min_y];
-
-            if(!visited[nx][ny]&&dist[nx][ny] > new_distance){
-                dist[nx][ny] = new_distance;
-                visited[nx][ny] = true;
-                pq.push(make_pair(-new_distance, make_pair(nx,ny)));
+                if(dist[nx][ny] > new_distance){
+                    dist[nx][ny] = new_distance;
+                    pq.push(make_pair(-new_distance, make_pair(nx,ny)));
+                }
             } 
         }
     }
