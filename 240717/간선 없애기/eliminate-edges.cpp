8244d9cd0b4b63f550798dbaf930d6e4f3db91d2
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-17 16:35:12 +0900

    Title: 간선 없애기, Time: 1016ms, Memory: 2MB, Status: Time Limit Exceed - CodeTree

---
+++ 240717/간선 없애기/eliminate-edges.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+int n,m;
+int arr[1001][1001];
+int dist[1001];
+int path[1001];
+bool visited[1001];
+
+void dijkstra(){
+    memset(visited, false, sizeof(visited));
+    for(int i=1; i<=n; i++)
+        dist[i] = 987654321;
+    dist[1] = 0;
+
+    for(int i=1; i<=n; i++){
+        int x = 987654321;
+        int idx = -1;
+
+        for(int j=1; j<=n; j++){
+            if(!visited[j]&&dist[j] < x){
+                idx = j;
+                x = dist[j];
+            }
+        }
+
+        if(idx==-1)
+            break;
+        
+        visited[idx] = true;
+
+        for(int j=1; j<=n; j++){
+            if(!visited[j]&&arr[idx][j]!=0&&dist[j] > dist[idx] + arr[idx][j]){
+                dist[j] = dist[idx] + arr[idx][j];
+                path[j] = idx;
+            }
+        }
+    }
+}
+
+int main() {
+    cin>>n>>m;
+
+    for(int i=0; i<m; i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        arr[a][b] = c;
+        arr[b][a] = c;
+    }
+
+    dijkstra();
+    int num = dist[n];
+    vector<pair<int,int>> a; 
+    int ans = 0;
+
+    for(int i=2; i<=n; i++)
+        a.push_back(make_pair(i,path[i]));
+
+    for(int i=0; i<a.size(); i++){
+        int x = a[i].first;
+        int y = a[i].second;
+        int number = arr[x][y];
+
+        arr[x][y] = 0;
+        arr[y][x] = 0;
+
+        dijkstra();
+        if(num!=dist[n])
+            ans++;
+        
+
+        arr[x][y] = number;
+        arr[y][x] = number;
+    }
+
+    cout<<ans;
+    return 0;
+}
