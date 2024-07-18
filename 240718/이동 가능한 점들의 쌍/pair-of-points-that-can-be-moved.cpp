d428bdb63e1ad689bfc5bafb25ec344d8b05caa3
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-18 14:04:24 +0900

    Title: 이동 가능한 점들의 쌍, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240718/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,m,p,q;
+    cin>>n>>m>>p>>q;
+
+    int arr[201][201];
+    bool visited[201][201] = {false};
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(i<=p||j<=p)
+                visited[i][j] = true;
+        }
+    }
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            arr[i][j] = (int)1e9;
+        }
+        arr[i][i] = 0;
+    }
+
+    for(int i=0; i<m; i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        arr[a][b] = c;
+        if(a<=p||b<=p)
+            visited[a][b] = true;
+    }
+
+    int ans = 0;
+    int cnt = 0;
+
+    for(int k=1; k<=n; k++){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(arr[i][j]>arr[i][k]+arr[k][j]){
+                    arr[i][j] = arr[i][k] + arr[k][j];
+
+                    if(k<=p||visited[i][k]||visited[k][j])
+                        visited[i][j] = true;
+                }
+            }
+        }
+    }
+
+    for(int i=0; i<q; i++){
+        int a,b;
+        cin>>a>>b;
+        if(visited[a][b]){
+            ans++;
+            cnt+=arr[a][b];
+        }
+    }
+    
+    cout<<ans<<'\n';
+    cout<<cnt<<'\n';
+    return 0;
+}
