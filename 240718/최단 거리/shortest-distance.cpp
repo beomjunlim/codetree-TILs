b4e1df1c0a4826f910a696cc40f03b0bf97f00e8
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-18 13:32:38 +0900

    Title: 최단 거리, Time: 24ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240718/최단 거리/shortest-distance.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+
+    int arr[101][101];
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int k=1; k<=n; k++){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                arr[i][j] = min(arr[i][j], arr[i][k] + arr[k][j]);
+            }
+        }
+    }
+
+    for(int i=0; i<m; i++){
+        int a,b;
+        cin>>a>>b;
+        cout<<arr[a][b]<<'\n';
+    }
+    return 0;
+}
