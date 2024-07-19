a66dcf598949427b8d3cfe0845745edfafd77d03
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-19 21:55:55 +0900

    Title: 별이 있는 숫자로만의 이동, Time: 684ms, Memory: 1MB, Status: Passed - CodeTree

--- 240719/별이 있는 숫자로만의 이동/move-to-numbers-with-star.cpp
+++ 240719/별이 있는 숫자로만의 이동/move-to-numbers-with-star.cpp
@@ -3,59 +3,39 @@
 #include <cmath>
 using namespace std;
 
-int n, m;
+int n,m;
 int arr[401][401];
-long long sum[401][401] = {0};
-
-bool InRange(int x, int y){
-    return 0 < x && x <= n && 0 < y && y <= n;
-}
-
-void ComputePrefixSum() {
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            sum[i][j] = arr[i][j] + sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1];
-        }
-    }
-}
-
-long long GetSum(int x1, int y1, int x2, int y2) {
-    if (x1 > x2 || y1 > y2) return 0;
-    return sum[x2][y2] - sum[x1-1][y2] - sum[x2][y1-1] + sum[x1-1][y1-1];
-}
-
-long long GetArea(int x, int y) {
-    long long sum = 0;
-    for (int dx = -m; dx <= m; dx++) {
-        int x1 = x + dx;
-        if (!InRange(x1, y)) continue;
-        int dy = m - abs(dx);
-        int y1 = max(1, y - dy);
-        int y2 = min(n, y + dy);
-        sum += GetSum(x1, y1, x1, y2);
-    }
-    return sum;
-}
+int sum[401][401];
 
 int main() {
-    cin >> n >> m;
+    cin>>n>>m;
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            cin >> arr[i][j];
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            cin>>arr[i][j];
+        }
+    }    
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            sum[i][j] = sum[i][j-1] + arr[i][j];
         }
     }
 
-    ComputePrefixSum();
+    int ans = 0;
 
-    long long ans = 0;
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            ans = max(ans, GetArea(i, j));
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            int sum_all = 0;
+            for(int r=i-m; r<=i+m; r++){
+                int c = m - abs(i-r);
+                if(1<=r&r<=n)
+                    sum_all += (sum[r][min(j+c,n)] - sum[r][max(j-c-1,0)]);
+            }
+            ans = max(ans, sum_all);
         }
     }
 
-    cout << ans;
-    return 0;
+    cout<<ans;
+   return 0;
 }
