69500cba63e0579d6849d15d7269d17851f95cad
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-19 18:44:27 +0900

    Title: 최대 직사각형 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240719/최대 직사각형 합/max-rect-sum-in-grid.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int arr[301][301] = {0};
+    int sum[301][301] = {0};
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    sum[1][1] = arr[1][1];
+    
+    for(int i=2; i<=n; i++){
+        sum[1][i] = sum[1][i-1] + arr[1][i];
+        sum[i][1] = sum[i-1][1] + arr[i][1];
+    }
+
+    for(int i=2; i<=n; i++){
+        for(int j=2; j<=n; j++){
+            sum[i][j] = arr[i][j] + sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1];
+        }
+    }
+
+    int ans = 0;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            for(int k=i; k<=n; k++){
+                for(int l=j; l<=n; l++){
+                    int cnt = sum[k][l] - sum[k-i][l] - sum[k][l-j] + sum[k-i][l-j];
+                    ans = max(ans, cnt);
+                }
+            }
+        }
+    }
+    cout<<ans;
+    return 0;
+}
