0e1b25c045268d3336d435e19e608f64ce80a85b
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-20 17:38:52 +0900

    Title: 겹치는 선분들, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240720/겹치는 선분들/overlapping-lines.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n,k;
+    cin>>n>>k;
+
+    vector<pair<int,int>> arr;
+    vector<pair<int,int>> sum;
+
+    int idx = 0;
+    for(int i=0; i<n; i++){
+        int a;
+        char b;
+        cin>>a>>b;
+        if(b=='L'){
+            arr.push_back(make_pair(idx-a, idx));
+            idx = idx - a;
+        }
+        else{
+            arr.push_back(make_pair(idx, idx+a));
+            idx = idx + a;
+        }
+    }
+
+    sort(arr.begin(), arr.end());
+
+    for(int i=0; i<n; i++){
+        int x = arr[i].first;
+        int y = arr[i].second;
+
+        sum.push_back(make_pair(x,1));
+        sum.push_back(make_pair(y,-1));
+    }
+
+    sort(sum.begin(), sum.end());
+
+    int sum_val = 0;
+    int ans = 0;
+    for(int i=0; i<2*n; i++){
+        int x = sum[i].first;
+        int y = sum[i].second;
+
+        sum_val += y;
+        if(sum_val>=k)
+            ans++;
+    }
+    cout<<ans;
+    return 0;
+}
