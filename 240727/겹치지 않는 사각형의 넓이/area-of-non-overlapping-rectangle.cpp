e80ed0028ecc430e91cf364d378e19dde18ad4fa
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-27 14:55:35 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 89ms, Memory: 15MB, Status: Passed - CodeTree

--- 240727/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
+++ 240727/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
@@ -7,6 +7,10 @@ int main() {
     for(int i=0; i<2; i++){
         int x1,x2,y1,y2;
         cin>>x1>>y1>>x2>>y2;
+        x1 += 1000;
+        y1 += 1000;
+        x2 += 1000;
+        y2 += 1000;
         for(int j=x1; j<x2; j++){
             for(int k=y1; k<y2; k++){
                 arr[k][j] = 1;
@@ -16,6 +20,10 @@ int main() {
 
     int x1,x2,y1,y2;
     cin>>x1>>y1>>x2>>y2;
+    x1 += 1000;
+    y1 += 1000;
+    x2 += 1000;
+    y2 += 1000;
     for(int i=x1; i<x2; i++){
         for(int j=y1; j<y2; j++){
             arr[j][i] = 0;
