7b854ec780abdec83c44bbaf233dfe615e07f026
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-07-28 16:00:46 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240728/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
+++ 240728/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
@@ -3,21 +3,20 @@ using namespace std;
 
 int a,b,c;
 int ans = 0;
-void function(int num){
-
-    if(num<=c)
-        ans = max(ans, num);
-    
-    if(num>c)
-        return;
-    function(num + a);
-    function(num + b);
-} 
 
 int main() {    
     cin>>a>>b>>c;
-    function(0);
+    int A = c/a + 1;
+    int B = c/b + 1;
+
+    for(int i=0; i<A; i++){
+        for(int j=0; j<B; j++){
+            int num = a*i + b*j;
+            if(num<=c)
+                ans = max(ans, num);
+        }
+    }
+
     cout<<ans;
-    
     return 0;
 }
