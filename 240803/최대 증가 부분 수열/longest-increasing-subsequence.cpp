f90dae4c292b87db32b602f7eec2cebbd994ef36
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-03 20:37:24 +0900

    Title: 최대 증가 부분 수열, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240803/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int arr[1001];
+int dp[1001];
+
+int main() {
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+        dp[i] = 1;
+    }
+
+    for(int i=1; i<n; i++){
+        for(int j=0; j<=i; j++){
+            if(arr[i]>arr[j])
+                dp[i] = max(dp[i], dp[j] + 1);
+        }
+    }
+
+    int ans = 0;
+    for(int i=0; i<n; i++)
+        ans = max(ans, dp[i]);
+    
+    cout<<ans;
+    return 0;
+}
