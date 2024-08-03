46cccaf8c10d0b0197a55e6517a2c2fe1b270ca8
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-03 19:43:10 +0900

    Title: 빙하, Time: 92ms, Memory: 0MB, Status: Passed - CodeTree

--- 240803/빙하/glacier.cpp
+++ 240803/빙하/glacier.cpp
@@ -1,76 +1,88 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
+#include <cstring>
 #include <queue>
-
 using namespace std;
 
-int N, M;
-vector<vector<int>> grid;
-vector<vector<bool>> visited;
-int dx[4] = {-1, 1, 0, 0};
-int dy[4] = {0, 0, -1, 1};
+int n,m;
+int ice = 0;
+int arr[201][201];
+bool visited[201][201];
+int dx[] = {0,0,-1,1};
+int dy[] = {-1,1,0,0};
+int size = 0;
 
-bool isInside(int x, int y) {
-    return x >= 0 && x < N && y >= 0 && y < M;
+bool InRange(int x, int y){
+    return 0<=x&&x<n&&0<=y&&y<m;
 }
 
-vector<pair<int, int>> bfs() {
-    queue<pair<int, int>> q;
-    q.push({0, 0});
+void bfs(){
+    memset(visited, false, sizeof(visited));
+    queue<pair<int,int>> q;
+    q.push(make_pair(0,0));
     visited[0][0] = true;
-    vector<pair<int, int>> melting_positions;
 
-    while (!q.empty()) {
+    while(!q.empty()){
         int x = q.front().first;
         int y = q.front().second;
         q.pop();
 
-        for (int i = 0; i < 4; ++i) {
+        for(int i=0; i<4; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (isInside(nx, ny) && !visited[nx][ny]) {
-                if (grid[nx][ny] == 0) {
-                    visited[nx][ny] = true;
-                    q.push({nx, ny});
-                } else if (grid[nx][ny] == 1) {
-                    visited[nx][ny] = true;
-                    melting_positions.push_back({nx, ny});
-                }
+            if(InRange(nx,ny)&&!visited[nx][ny]&&arr[nx][ny]==0){
+                visited[nx][ny] = true;
+                q.push(make_pair(nx,ny));
             }
         }
     }
 
-    return melting_positions;
+    int cnt = 0;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(arr[i][j]==1){
+                bool sw = false;
+                for(int k=0; k<4; k++){
+                    int x = i + dx[k];
+                    int y = j + dy[k];
+                    if(InRange(x,y)&&visited[x][y]){
+                        sw = true;
+                        break;
+                    }
+                }
+
+                if(sw){
+                    arr[i][j] = 0;
+                    cnt++;
+                }
+            }
+        }
+    }
+    size = cnt;
+    ice -= cnt;
 }
 
-int main() {
-    cin >> N >> M;
-    grid.resize(N, vector<int>(M));
-    visited.resize(N, vector<bool>(M, false));
 
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < M; ++j) {
-            cin >> grid[i][j];
+int main() {
+    cin>>n>>m;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>arr[i][j];
+            if(arr[i][j]==1){
+                ice++;
+            }
         }
     }
 
     int time = 0;
-    int last_melting_size = 0;
-
-    while (true) {
-        visited.assign(N, vector<bool>(M, false));
-        vector<pair<int, int>> melting_positions = bfs();
-        if (melting_positions.empty()) {
-            break;
-        }
-        last_melting_size = melting_positions.size();
-        for (auto pos : melting_positions) {
-            grid[pos.first][pos.second] = 0;
-        }
+    while(ice!=0){
         time++;
+        bfs();
     }
 
-    cout << time << " " << last_melting_size << endl;
+    cout<<time<<" "<<size<<'\n';
     return 0;
 }
