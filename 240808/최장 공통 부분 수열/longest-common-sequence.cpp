d3e46751a8971dd14d34a1b41661359ad40fa5f5
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-08 13:47:29 +0900

    Title: 최장 공통 부분 수열, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240808/최장 공통 부분 수열/longest-common-sequence.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string a,b;
+int dp[1001][1001];
+
+int main() {
+    cin>>a>>b;
+
+    a = " " + a;
+    b = " " + b;
+
+    int A = a.length();
+    int B = b.length();
+
+    if(a[1]==b[1])
+        dp[1][1] = 1;
+    else
+        dp[1][1] = 0;
+    
+    for(int i=1; i<A; i++){
+        if(a[i]==b[1])
+            dp[i][1] = 1;
+        else
+            dp[i][1] = 0;
+    }
+
+    for(int i=1; i<B; i++){
+        if(b[i]==a[1])
+            dp[1][i] = 1;
+        else
+            dp[1][i] = 0;
+    }
+
+    for(int i=2; i<A; i++){
+        for(int j=2; j<B; j++){
+            if(a[i]==b[j])
+                dp[i][j] = max(dp[i-1][j-1] + 1, max(dp[i-1][j], dp[i][j-1]));
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+
+    cout<<dp[A-1][B-1];
+    return 0;
+}
