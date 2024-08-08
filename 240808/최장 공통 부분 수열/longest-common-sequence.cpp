859d5b130948dd5e9c558d206b4169046ddedcdd
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-08 13:49:22 +0900

    Title: 최장 공통 부분 수열, Time: 13ms, Memory: 4MB, Status: Passed - CodeTree

--- 240808/최장 공통 부분 수열/longest-common-sequence.cpp
+++ 240808/최장 공통 부분 수열/longest-common-sequence.cpp
@@ -42,6 +42,14 @@ int main() {
         }
     }
 
-    cout<<dp[A-1][B-1];
+    int ans = 0;
+
+    for(int i=1; i<A; i++){
+        for(int j=1; j<B; j++){
+            ans = max(ans, dp[i][j]);        
+        }
+    }
+    
+    cout<<ans;
     return 0;
 }
