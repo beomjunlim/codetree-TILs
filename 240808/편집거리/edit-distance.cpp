8b7b73441ab8d34c5b9ed4ce35ca7b06e7954072
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-08 14:12:36 +0900

    Title: 편집거리, Time: 14ms, Memory: 4MB, Status: Passed - CodeTree

--- 240808/편집거리/edit-distance.cpp
+++ 240808/편집거리/edit-distance.cpp
@@ -38,7 +38,7 @@ int main() {
             if(b[i]==a[j])
                 dp[i][j] = dp[i-1][j-1];
             else
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + 1;
+                dp[i][j] = min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1])) + 1;
         }
     }
 
