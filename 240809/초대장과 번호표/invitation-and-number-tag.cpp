b0c83060f0cf878ce02bd2fe6422f435a7f19af9
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-09 14:50:25 +0900

    Title: 초대장과 번호표, Time: 487ms, Memory: 40MB, Status: Passed - CodeTree

--- 240809/초대장과 번호표/invitation-and-number-tag.cpp
+++ 240809/초대장과 번호표/invitation-and-number-tag.cpp
@@ -1,112 +1,53 @@
-// #include <iostream>
-// #include <vector>
-// #include <unordered_set>
-// using namespace std;
-
-// bool visited[100001];
-// vector<int> group[250001];
-
-// int main() {
-//     int n,g;
-//     cin>>n>>g;
-
-//     unordered_set<int> card;
-
-//     for(int i=0; i<g; i++){
-//         int num;
-//         cin>>num;
-//         for(int j=0; j<num; j++){
-//             int a;
-//             cin>>a;
-//             group[i].push_back(a);
-//             if(j==0)
-//                 card.insert(a);        
-//         }
-//     }
-
-
-
-//     return 0;
-// }
-
 #include <iostream>
 #include <vector>
 #include <unordered_set>
 #include <queue>
 using namespace std;
 
-int n, g;
-vector<vector<int>> groups; // 각 그룹의 멤버를 저장
-vector<unordered_set<int>> person_groups; // 각 사람의 그룹 목록을 저장
+int main() {
+    int n,g;
+    cin>>n>>g;
 
-// BFS 탐색을 통한 초대장 수신자 결정
-int find_invited_count() {
-    vector<bool> invited(n + 1, false);
+    unordered_set<int> group[250001];
+    unordered_set<int> people[100001];
+    unordered_set<int> card;
     queue<int> q;
 
-    // 그룹 1의 멤버들을 모두 초대장 받는 사람으로 설정
-    for (int person : groups[1]) {
-        invited[person] = true;
-        q.push(person);
+    for(int i=1; i<=g; i++){
+        int num;
+        cin>>num;
+        for(int j=0; j<num; j++){
+            int a;
+            cin>>a;
+
+            group[i].insert(a);
+            people[a].insert(i);
+        }
     }
 
-    while (!q.empty()) {
-        int person = q.front();
+    q.push(1);
+    card.insert(1);
+    
+    while(!q.empty()){
+        int x = q.front();
         q.pop();
 
-        // 사람 `person`이 속한 그룹을 확인
-        for (int group_id : person_groups[person]) {
-            int count_invited = 0;
-            vector<int> members;
+        for(auto i : people[x]){
+            int G = i;
+            if(group[G].find(x)!=group[G].end()){
+                group[G].erase(x);
 
-            // 그룹의 모든 멤버를 확인
-            for (int member : groups[group_id]) {
-                members.push_back(member);
-                if (invited[member]) {
-                    count_invited++;
-                }
-            }
-
-            // 그룹의 멤버 수가 초대장을 받은 멤버 수보다 1명 더 많다면
-            if (count_invited == members.size() - 1) {
-                for (int member : members) {
-                    if (!invited[member]) {
-                        invited[member] = true;
-                        q.push(member);
+                if(group[G].size()==1){
+                    int y = *group[G].begin();
+                    if(card.find(y)==card.end()){
+                        q.push(y);
+                        card.insert(y);
                     }
                 }
             }
         }
     }
 
-    // 초대장을 받은 사람 수 계산
-    int count = 0;
-    for (int i = 1; i <= n; i++) {
-        if (invited[i]) {
-            count++;
-        }
-    }
-    return count;
-}
-
-int main() {
-    cin >> n >> g;
-
-    groups.resize(g + 1); // 그룹 1부터 G까지
-    person_groups.resize(n + 1); // 사람 1부터 N까지
-
-    for (int i = 1; i <= g; i++) {
-        int k;
-        cin >> k;
-        for (int j = 0; j < k; j++) {
-            int person;
-            cin >> person;
-            groups[i].push_back(person);
-            person_groups[person].insert(i);
-        }
-    }
-
-    cout << find_invited_count() << endl;
-
+    cout<<card.size();
     return 0;
 }
