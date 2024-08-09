b64362ba4c2c95b39c81b612a4d7b91548c09079
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-09 15:20:51 +0900

    Title: 가까운 숫자, Time: 130ms, Memory: 2MB, Status: Wrong Answer - CodeTree

---
+++ 240809/가까운 숫자/nearest-number.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+
+    set<int> s;
+    s.insert(0);
+
+    int ans = 1e9;
+
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        int num;
+        cin>>num;
+
+        auto it = s.upper_bound(num);
+        if(it==s.end()){
+            int A = *s.begin();
+            ans = min(ans, abs(num-A));
+        }
+        else{
+            int A = *it;
+            if(it!=s.begin()){
+                int B = *prev(it);
+                ans = min(ans, abs(num-B));
+            }
+            ans = min(ans, abs(num-A));
+        }
+        s.insert(num);
+        cout<<ans<<'\n';
+    }
+    return 0;
+}
