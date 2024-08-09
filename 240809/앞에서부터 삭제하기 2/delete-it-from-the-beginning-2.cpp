d9c28107eefb493ee867bc1fa424d9da77af5bab
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-09 20:49:01 +0900

    Title: 앞에서부터 삭제하기 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <queue>
+#include <iomanip>
+#include <algorithm>
+using namespace std;
+
+int arr[100001];
+int dp[100001];
+
+int main() {
+    int n;
+    cin>>n;
+
+    double ans = 0;
+    priority_queue<pair<int,int>> pq;
+
+    for(int i=1; i<=n; i++){
+        cin>>arr[i];
+        dp[i] = dp[i-1] + arr[i];
+        pq.push(make_pair(-arr[i],i));
+    }
+
+    for(int i=1; i<=n-2; i++){
+        double sum = dp[n] - dp[i];
+        int num = pq.top().first;
+        int idx = pq.top().second;
+        while(idx<i){
+            pq.pop();
+            num = pq.top().first;
+            idx = pq.top().second;
+        }
+
+        sum += num;
+        int cnt = n - i - 1;
+        ans = max(ans, (double)sum/cnt);
+    }
+
+    cout<<fixed<<setprecision(2);
+    cout<<ans;
+    return 0;
+}
