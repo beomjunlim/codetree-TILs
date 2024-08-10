3a9a7112d1ecce30a07be213b99354b37bf153d4
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-10 12:53:12 +0900

    Title: 중앙값, Time: 231ms, Memory: 1MB, Status: Passed - CodeTree

--- 240810/중앙값/median.cpp
+++ 240810/중앙값/median.cpp
@@ -7,6 +7,77 @@ int main() {
     int t;
     cin>>t;
 
+    // while(t>0){
+    //     int m;
+    //     cin>>m;
+        
+    //     priority_queue<int> big;
+    //     priority_queue<int> small;
+    //     int mid;
+
+    //     for(int i=1; i<=m; i++){
+    //         int num;
+    //         cin>>num;
+    //         if(i==1){
+    //             mid = num;
+    //             cout<<num<<" ";
+    //         }
+    //         else if(i%2==0){
+    //             if(mid>num)
+    //                 small.push(num);
+    //             else
+    //                 big.push(-num);
+    //         }
+    //         else if(i%2==1){
+    //             if(num>mid){
+    //                 if(big.empty()){
+    //                     big.push(-num);
+    //                     cout<<mid<<" ";
+    //                 }
+    //                 else{
+    //                     int A = -big.top();
+    //                     if(A>num){
+    //                         small.push(mid);
+    //                         mid = num;
+    //                         cout<<mid<<" ";
+    //                     }
+    //                     else{
+    //                         small.push(mid);
+    //                         big.pop();
+    //                         big.push(-num);
+    //                         mid = A;
+    //                         cout<<mid<<" ";
+    //                     }
+    //                 }
+    //             }
+    //             else{
+    //                 if(small.empty()){
+    //                     small.push(num);
+    //                     cout<<mid<<" ";
+    //                 }
+    //                 else{
+    //                     int A = small.top();
+    //                     if(A>num){
+    //                         small.pop();
+    //                         small.push(num);
+    //                         big.push(-mid);
+    //                         mid = A;
+    //                         cout<<mid<<" ";
+    //                     }
+    //                     else{
+    //                         big.push(-mid);
+    //                         mid = num;
+    //                         cout<<mid<<" ";
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     t--;
+    //     cout<<'\n';
+    // }
+
+
     while(t>0){
         int m;
         cin>>m;
@@ -18,63 +89,63 @@ int main() {
         for(int i=1; i<=m; i++){
             int num;
             cin>>num;
+
             if(i==1){
                 mid = num;
                 cout<<num<<" ";
             }
-            else if(i%2==0){
-                if(mid>num)
-                    small.push(num);
-                else
-                    big.push(-num);
-            }
-            else if(i%2==1){
-                if(num>mid){
-                    if(big.empty()){
+            else{
+                if(i%2==0){
+                    if(mid>num)
+                        small.push(num);
+                    else
                         big.push(-num);
-                        cout<<mid<<" ";
-                    }
-                    else{
-                        int A = -big.top();
-                        if(A>num){
-                            small.push(mid);
-                            mid = num;
-                            cout<<mid<<" ";
-                        }
-                        else{
-                            small.push(mid);
-                            big.pop();
-                            big.push(-num);
-                            mid = A;
-                            cout<<mid<<" ";
-                        }
-                    }
                 }
                 else{
-                    if(small.empty()){
-                        small.push(num);
-                        cout<<mid<<" ";
-                    }
-                    else{
-                        int A = small.top();
-                        if(A>num){
-                            small.pop();
-                            small.push(num);
+                    if(small.size()>big.size()){
+                        if(mid>num){
                             big.push(-mid);
-                            mid = A;
-                            cout<<mid<<" ";
+                            int A = small.top();
+
+                            if(A>num){
+                                small.pop();
+                                mid = A;
+                                small.push(num);
+                            }
+                            else{
+                                mid = num;
+                            }
                         }
                         else{
-                            big.push(-mid);
-                            mid = num;
-                            cout<<mid<<" ";
+                            big.push(-num);
                         }
                     }
+                    else{
+                        if(mid<num){
+                            small.push(mid);
+                            int A = -big.top();
+
+                            if(A<num){
+                                big.pop();
+                                mid = A;
+                                big.push(-num);
+                            }
+                            else{
+                                mid = num;
+                            }
+                        }
+                        else{
+                            small.push(num);
+                        }
+                    }
+                    cout<<mid<<" ";
                 }
             }
         }
         t--;
         cout<<'\n';
     }
+
+
     return 0;
 }
