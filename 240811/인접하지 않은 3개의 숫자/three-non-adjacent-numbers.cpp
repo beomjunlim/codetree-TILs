e63d5ecb0ba4f3a7e20e20d299efd088b82be89b
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-11 20:01:52 +0900

    Title: 인접하지 않은 3개의 숫자, Time: 30ms, Memory: 1MB, Status: Passed - CodeTree

--- 240811/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.cpp
+++ 240811/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.cpp
@@ -3,31 +3,34 @@
 using namespace std;
 
 int arr[100001];
-int dp[100001][3][2];
+int dp[100001][4];
 
 int main() {
     int n;
-    cin>>n;
+    cin >> n;
 
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
     }
 
-    dp[0][0][0] = 0;
-    dp[0][1][1] = arr[0];
+    for (int i = 0; i <= n; i++) {
+        dp[i][0] = 
+        dp[i][1] = dp[i][2] = dp[i][3] = -1e9;
+    }
 
-    for(int i=1; i<n; i++){
-        for(int j=1; j<=3; j++){
-            dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1]);
-            dp[i][j][1] = max(dp[i][j][1], dp[i-1][j-1][0] + arr[i]);
+    for (int i = 1; i <= n; i++) {
+        dp[i][0] = 0; 
+        dp[i][1] = max(dp[i-1][1], arr[i]);
+
+        if (i >= 2) {
+            dp[i][2] = max(dp[i-1][2], dp[i-2][1] + arr[i]);
+        }
+        if (i >= 3) {
+            dp[i][3] = max(dp[i-1][3], dp[i-2][2] + arr[i]);
         }
     }
 
-    int ans = 0;
-    for(int i=1; i<=n; i++){
-        ans = max(ans, max(dp[i][3][0], dp[i][3][1]));
-    }
+    cout << dp[n][3] << endl;
 
-    cout<<ans;
     return 0;
 }
