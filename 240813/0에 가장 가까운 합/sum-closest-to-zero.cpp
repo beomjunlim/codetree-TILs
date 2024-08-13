40c7b8613917a6bae1a1345391a5af10c9a6a2a4
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-13 20:11:59 +0900

    Title: 0에 가장 가까운 합, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240813/0에 가장 가까운 합/sum-closest-to-zero.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int arr[100001];
+
+int main() {
+    int n;
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        cin>>arr[i];
+    }    
+
+    sort(arr, arr+n);
+
+    int ans = 1e9;
+    int j=n;
+    for(int i=1; i<=n; i++){
+        if(i<j)
+            ans = min(ans, abs(arr[i] + arr[j]));
+        while(i<j&&arr[i] + arr[j]>0){
+            ans = min(ans, abs(arr[i] + arr[j]));
+            j--;
+        }
+    }
+
+    cout<<ans;
+
+    return 0;
+}
