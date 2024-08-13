cfc2260e8442292a33f35bf651247911fce3acf4
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-13 16:30:36 +0900

    Title: 정수 두 개의 합 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240813/정수 두 개의 합 2/sum-of-two-integers-2.cpp
+++ 240813/정수 두 개의 합 2/sum-of-two-integers-2.cpp
@@ -5,32 +5,38 @@ using namespace std;
 int arr[100001];
 
 int main() {
-    int n,k;
-    cin>>n>>k;
+    int n, k;
+    cin >> n >> k;
 
-    for(int i=0; i<n; i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
 
-    sort(arr, arr+n);
+    sort(arr, arr + n);
+
     int ans = 0;
-    int start = 0;
-    int end = start + 1;
-    while(start<end){
-        int sum = arr[start] + arr[end];
+    int i = 0, j = n - 1;
 
-        if(sum<=k){
-            ans++;
-            if(end==n-1)
-                start++;
-            else
-                end++;
-        }
-        else if(sum>k){
-            start++;
+    while (i < j) {
+        if (arr[i] + arr[j] <= k) {
+            ans += (j - i); // j와의 쌍 모두 카운트
+            i++; // i를 증가시켜 더 큰 값을 고려
+        } else {
+            j--; // j를 감소시켜 더 작은 값을 고려
         }
     }
 
-    cout<<ans;
+
+    int j = 0;
+    int i = n-1;
+
+    while(j<i){
+        while(arr[i] + arr[j]<=k){
+            j++;
+        }
+        ans += j;
+        i--;
+    }
+    cout << ans;
     return 0;
 }
