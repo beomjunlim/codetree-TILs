dbcd056c38968b3b58af45fa89059af80db3ba61
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-15 14:20:29 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240815/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int visited[50001];
+int B[50001];
+
+int main() {
+    int n;
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        cin>>B[i];
+        visited[B[i]] = true;
+    }
+
+    set<int> A;
+
+    for(int i=1; i<=2*n; i++){
+        if(!visited[i])
+            A.insert(i);
+    }
+
+
+    int ans = 0;
+    for(int i=0; i<n; i++){
+        auto it = A.upper_bound(B[i]);
+        if(it!=A.end()){
+            int num = *it;
+            ans++;
+            A.erase(num);
+        }
+    }
+
+    cout<<ans;
+    return 0;
+}
