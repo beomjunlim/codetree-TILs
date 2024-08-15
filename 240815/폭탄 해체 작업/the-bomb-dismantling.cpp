3f1f9c31ef6b0593a7d7b84b5462d0a6d36b6450
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-15 15:01:09 +0900

    Title: 폭탄 해체 작업, Time: 15ms, Memory: 0MB, Status: Passed - CodeTree

--- 240815/폭탄 해체 작업/the-bomb-dismantling.cpp
+++ 240815/폭탄 해체 작업/the-bomb-dismantling.cpp
@@ -1,37 +1,46 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
 bool cmp(pair<int,int> a, pair<int,int> b){
-    if(a.first==b.first)
-        return a.second > b.second;
-    return a.first < b.first;
+    if(a.second==b.second)
+        return a.first > b.first;
+    return a.second > b.second;
 }
 
 int main() {
     int n;
     cin>>n;
 
-    vector<pair<int,int>> v;
-
+    int max_time = 0;
+    vector<pair<int,int>> bomb;
     for(int i=0; i<n; i++){
-        int s,t;
-        cin>>s>>t;
-        v.push_back(make_pair(t,s));
+        int score,time;
+        cin>>score>>time;
+        bomb.push_back(make_pair(time, score));
+        max_time = max(max_time, time);
     }
 
-    sort(v.begin(), v.end(), cmp);
+    sort(bomb.begin(), bomb.end());
 
-    int score = 0;
-    int time = 0;
-    for(int i=0; i<n; i++){
-        if(time<v[i].first){
-            time = v[i].first;
-            score += v[i].second;
+    priority_queue<int> pq;
+    int idx = n-1;
+    int ans = 0;
+
+    for(int i=max_time; i>0; i--){
+        while(idx>=0&&bomb[idx].first>=i){
+            pq.push(bomb[idx].second);
+            idx--;
+        }
+
+        if(!pq.empty()){
+            ans += pq.top();
+            pq.pop();
         }
     }
-
-    cout<<score<<'\n';
+ 
+    cout<<ans;
     return 0;
 }
