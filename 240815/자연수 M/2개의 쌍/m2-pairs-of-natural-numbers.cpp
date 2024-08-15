f68eed2f2589b059cfa8a11c35bd0689e97df90d
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-15 15:48:05 +0900

    Title: 자연수 M/2개의 쌍, Time: 4ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240815/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+
+    vector<pair<int,int>> v;
+    for(int i=0; i<n; i++){
+        int x,y;
+        cin>>x>>y;
+        v.push_back(make_pair(y,x));
+    }
+
+    sort(v.begin(), v.end());
+
+    int left = 0;
+    int right = n-1;
+    int ans = 1e9;
+
+    while(left<=right){
+        int A = v[left].first;
+        int A_num = v[left].second;
+
+        int B = v[right].first;
+        int B_num = v[right].second;
+
+        ans = min(ans, A+B);
+
+        if(A_num<B_num){
+            v[right].second = B_num - A_num;
+            left++;
+        }
+        else if(A_num>B_num){
+            v[left].second = A_num - B_num;
+            right--;
+        }
+        else{
+            left++;
+            right--;
+        }
+    }
+    cout<<ans;
+    return 0;
+}
