1d03ee52d2b36f244e5b78e74fbedff1eb767bd8
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-15 09:29:57 +0900

    Title: 자연수 n개의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240815/자연수 n개의 합/sum-of-n-natural-numbers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+long long s;
+
+int main() {
+    cin >> s;
+
+    long long left = 1;                   
+    long long right = s;             
+    long long max_num = 0;        
+
+    while (left <= right) {                     
+        long long mid = (left + right) / 2;      
+        if(mid * (mid + 1) / 2 <= s) {          
+            left = mid + 1;                   
+            max_num = max(max_num, mid);      
+        }
+        else                               
+            right = mid - 1;                
+    }
+
+    cout << max_num;                    
+    return 0;
+}
