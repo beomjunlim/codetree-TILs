890c269e5e2066dcbe6a7be457f8066851de233c
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-15 14:10:05 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 49ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240815/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[100001];
+int L[100001];
+int R[100001];
+
+int main() {
+    int n;
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        cin>>arr[i];
+    }
+
+    L[1] = arr[1];
+    for(int i=2; i<=n; i++){
+        L[i] = min(L[i-1], arr[i]);
+    }
+
+    R[n] = arr[n];
+    for(int i=n-1; i>0; i--){
+        R[i] = max(R[i+1], arr[i]);
+    }
+
+    int ans = 0;
+    for(int i=1; i<n; i++){
+        ans = max(ans, R[i+1] - L[i]);
+    }
+    
+    cout<<ans;
+    return 0;
+}
