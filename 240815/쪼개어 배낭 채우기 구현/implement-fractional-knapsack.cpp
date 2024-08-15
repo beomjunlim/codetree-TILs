e42840ac55022dcc33134d407758ee76cecd3a93
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-15 13:19:03 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 74ms, Memory: 1MB, Status: Runtime Error - CodeTree

---
+++ 240815/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin>>n>>m;
+
+    vector<pair<double,int>> arr;
+    for(int i=0; i<n; i++){
+        double a,b;
+        cin>>a>>b;
+        arr.push_back(make_pair(b/a,a));
+    }
+
+    sort(arr.begin(), arr.end());
+
+    int bag = m;
+    int idx = n-1;
+    double value = 0;
+    while(bag>0){
+        if(bag>=arr[idx].second){
+            value += arr[idx].first*arr[idx].second;
+            bag -= arr[idx].second;
+        }
+        else{
+            value += arr[idx].first*bag;
+            break;
+        }
+        idx--;
+    }
+    
+    cout<<fixed<<setprecision(3)<<value;
+    return 0;
+}
