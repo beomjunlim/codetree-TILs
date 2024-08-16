f9393280f4596f03fff8ffea82d41ad325fe1350
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-16 13:03:57 +0900

    Title: 각 정점까지의 최단 경로 3, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240816/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.cpp
+++ 240816/각 정점까지의 최단 경로 3/shortest-path-to-each-vertex-3.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int n,m;
 int cost[101];
+bool visited[101];
 vector<pair<int,int>> v[101];
 
 int main() {
@@ -27,33 +28,49 @@ int main() {
         }
     }
 
-    priority_queue<pair<int,int>> pq;
+    // priority_queue<pair<int,int>> pq;
 
-    for(int i=2; i<=n; i++){
-        if(cost[i]!=INT_MAX){
-            pq.push(make_pair(-cost[i], i));
-        }
-    }
+    // for(int i=2; i<=n; i++){
+    //     if(cost[i]!=INT_MAX){
+    //         pq.push(make_pair(-cost[i], i));
+    //     }
+    // }
 
-    while(!pq.empty()){
-        int dist = -pq.top().first;
-        int node = pq.top().second;
-        pq.pop();
+    // while(!pq.empty()){
+    //     int dist = -pq.top().first;
+    //     int node = pq.top().second;
+    //     pq.pop();
 
-        if(cost[node]<dist)
-            continue;
+    //     if(cost[node]<dist)
+    //         continue;
         
-        for(int i=0; i<v[node].size(); i++){
-            int next_node = v[node][i].first;
-            int next_dist = v[node][i].second;
+    //     for(int i=0; i<v[node].size(); i++){
+    //         int next_node = v[node][i].first;
+    //         int next_dist = v[node][i].second;
 
-            if(next_dist + dist < cost[next_node]){
-                cost[next_node] = next_dist + dist;
-                pq.push(make_pair(-cost[next_node], next_node));
+    //         if(next_dist + dist < cost[next_node]){
+    //             cost[next_node] = next_dist + dist;
+    //             pq.push(make_pair(-cost[next_node], next_node));
+    //         }
+    //     }
+    // }
+    for(int i=1; i<=n; i++){
+        
+        int min_idx;
+        int dist = INT_MAX;
+        for(int j=1; j<=n; j++){
+            if(!visited[j]&&cost[j]<dist){
+                min_idx = j;
+                dist = cost[j];
             }
         }
-    }
 
+        visited[min_idx] = true;
+
+        for(int j=0; j<v[min_idx].size(); j++){
+            cost[v[min_idx][j].first] = min(cost[v[min_idx][j].first], cost[min_idx] + v[min_idx][j].second);
+        }
+    }
 
     for(int i=2; i<=n; i++){
         if(cost[i]==INT_MAX)
