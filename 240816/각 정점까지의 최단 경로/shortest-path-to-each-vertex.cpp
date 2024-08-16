241f7325b243b409e3cf97825d0b4aea78034131
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-16 18:41:10 +0900

    Title: 각 정점까지의 최단 경로, Time: 174ms, Memory: 5MB, Status: Wrong Answer - CodeTree

---
+++ 240816/각 정점까지의 최단 경로/shortest-path-to-each-vertex.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <queue>
+using namespace std;
+
+int n,m,k;
+int dist[20001];
+vector<pair<int,int>> v[20001];
+
+int main() {
+    cin>>n>>m;
+    cin>>k;
+
+    for(int i=1; i<=n; i++){
+        dist[i] = INT_MAX;
+    }
+
+    dist[k] = 0;
+
+    for(int i=0; i<m; i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        v[a].push_back(make_pair(c,b));
+        v[b].push_back(make_pair(b,a));
+
+        if(a==k)
+            dist[b] = c;
+        if(b==k)
+            dist[a] = c;
+    }
+
+    priority_queue<pair<int,int>> pq;
+    
+    for(int i=1; i<=n; i++){
+        if(dist[i]!=INT_MAX&&i!=k){
+            pq.push(make_pair(-dist[i], i));
+        }
+    }
+
+    while(!pq.empty()){
+        int dis = -pq.top().first;
+        int node = pq.top().second;
+        pq.pop();
+
+        if(dist[node]<dis)
+            continue;
+        
+        for(int i=0; i<v[node].size(); i++){
+            int next_node = v[node][i].second;
+            int next_dis = dis + v[node][i].first;
+
+            if(next_dis < dist[next_node]){
+                dist[next_node] = next_dis;
+                pq.push(make_pair(-next_dis, next_node));
+            }
+        }
+    }
+
+
+    for(int i=1; i<=n; i++){
+        if(dist[i]==INT_MAX)
+            cout<<-1<<'\n';
+        else
+            cout<<dist[i]<<'\n';
+    }
+    return 0;
+}
