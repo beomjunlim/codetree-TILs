5b91c96f1538a120683c6bc6a7efb9bad46daf24
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-16 11:46:56 +0900

    Title: G & H 반전시키기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240816/G & H 반전시키기 2/reversing-g-and-h-2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int A[1001];
+int B[1001];
+
+int main() {
+    int n;
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        char a;
+        cin>>a;
+
+        if(a=='G')
+            A[i] = 0;
+        if(a=='H')
+            A[i] = 1;
+    }
+
+    for(int i=0; i<n; i++){
+        char a;
+        cin>>a;
+
+        if(a=='G')
+            B[i] = 0;
+        if(a=='H')
+            B[i] = 1;
+    }
+
+    int cnt = 0;
+    for(int i=n-1; i>=0; i--){
+        if(A[i]!=B[i]){
+            cnt++;
+            for(int j=i; j>=0; j--){
+                A[j] ^= 1;
+            }
+        }
+    }
+
+    cout<<cnt;
+    return 0;
+}
