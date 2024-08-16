c768766ac0c5a0ec9028a02c1647f9f3d919eba8
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-16 11:30:43 +0900

    Title: 좌우 반전시키기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240816/좌우 반전시키기/flip-left-and-right.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int arr[103];
+
+int main() {
+    int n;
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int cnt = 0;
+    if(arr[0]==0){
+        cnt++;
+        arr[1] = 1 - arr[1];
+    }
+
+    for(int i=2; i<n; i++){
+        if(arr[i-1]==0){
+            cnt++;
+            arr[i] = 1 - arr[i];
+            arr[i+1] = 1 - arr[i];
+        }
+    }
+
+    if(arr[n-1]==0)
+        cnt = -1;
+    cout<<cnt;
+    return 0;
+}
