8af15d87f8def06343048e256b6e892326b527d6
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-08-22 16:16:15 +0900

    Title: 색깔 트리, Time: 440ms, Memory: 5MB, Status: Wrong Answer - CodeTree

--- 240822/색깔 트리/color-tree.cpp
+++ 240822/색깔 트리/color-tree.cpp
@@ -1,153 +1,107 @@
-#include <stdio.h>
+#include <iostream>
 #include <vector>
-
+#include <algorithm>
+#include <cstring>
+#include <map>
+#include <set>
 using namespace std;
 
-#define MAX (100000 + 5000)
-#define MAX_DEPTH (100 + 10)
-#define MAX_COLOR (5)
+map<int, pair<int,pair<int,int>>> node;
+map<int,set<int>>Parent;
+set<int> root;
+int score = 0;
+bool COLOR[6];
+bool visited_node[100001];
 
-#define ADD_NODE (100)
-#define CHANGE_COLOR (200)
-#define RETRIEVE_COLOR (300)
-#define RETRIEVE_SCORE (400)
+void dfs(int x) {
+    if (visited_node[x]) return; // 이미 방문한 노드일 경우 넘긴다.
 
-#define PARENT (1)
+    visited_node[x] = true; // 해당 노드 방문 처리
 
-typedef struct st
-{
-	int id;
-	int color;
-	int maxDepth;
-	int parent;
-	vector<int> child;
-}NODE;
+    bool localColor[6] = {false}; 
+    localColor[node[x].second.first] = true; // 해당 노드 색 true
 
-NODE node[MAX];
-int checkRoot[MAX];
+    for (auto it : Parent[x]) {
+        dfs(it); // 자식 노드 dfs 호출
+        for (int i = 1; i <=5; i++) {
+            if (COLOR[i]) { // 자식 노드에서 방문한 것 처리
+                localColor[i] = true;
+            }
+        }
+    }
 
-int checkMakeChild(NODE cur, int depth)
-{
-	if (cur.id == 0) return 1; // 최대 깊이 이전에 부모를 찾은 경우
-	if (cur.maxDepth <= depth) return 0;
-	return checkMakeChild(node[cur.parent], depth + 1);
+    for (int i = 1; i <=5; i++) {
+        COLOR[i] = localColor[i]; // 전역 배열 업데이트
+    }
+
+    int cnt = 0;
+    for (int i = 1; i <=5; i++) {
+        if (COLOR[i]) {
+            cnt++;
+        }
+    }
+
+    score += cnt * cnt;
 }
 
-void addNode(int m_id, int p_id, int color, int max_depth)
-{
-	if (p_id == -1) checkRoot[m_id] = PARENT;
+int main() {
+    int q;
+    cin>>q;
 
-	if (checkRoot[m_id] == PARENT || checkMakeChild(node[p_id], 1))
-	{
-		node[m_id].id = m_id;
-		node[m_id].color = color;
-		node[m_id].maxDepth = max_depth;
-		node[m_id].parent = checkRoot[m_id] == PARENT ? 0 : p_id;
+    for(int i=1; i<=q; i++){
+        int n;
+        cin>>n;
 
-		if (checkRoot[m_id] != PARENT)
-			node[p_id].child.push_back(m_id);
-	}
-}
+        if(n==100){
+            int id,Pid,color,depth;
+            cin>>id>>Pid>>color>>depth;
+            if(Pid==-1){
+                node[id] = make_pair(Pid, make_pair(color, depth));
+                root.insert(id);
+            }
+            else{
+                int P_depth = node[Pid].second.second;
+                depth = min(P_depth - 1, depth);
+                if(depth==0)
+                    continue;
+                node[id] = make_pair(Pid, make_pair(color, depth));
+                int parent = Pid;
+                while(parent!=-1){
+                    Parent[parent].insert(id); // 해당 부모가 가지고 있는 자식들
+                    parent = node[parent].first;
+                }
+            }
+        }
+        else if(n==200){
+            int id, color;
+            cin>>id>>color;
+            
+            int Pid = node[id].first;
+            int depth = node[id].second.second;
 
-void dfs(int id, int color)
-{
-	node[id].color = color;
+            node[id] = make_pair(Pid, make_pair(color, depth));
 
-	for (int childId : node[id].child)
-		dfs(childId, color);
-}
+            for(auto it : Parent[id]){
+                int Pid = node[it].first;
+                int depth = node[it].second.second;
+                node[it] = make_pair(Pid, make_pair(color, depth));
+            }
+        }
+        else if(n==300){
+            int id;
+            cin>>id;
+            cout<<node[id].second.first<<'\n';
+        }
+        else if(n==400){
+            score = 0;
+            for(auto it : root){
+                memset(visited_node, false, sizeof(visited_node));
+                memset(COLOR, false, sizeof(COLOR));
+                dfs(it);
+            }
+            cout<<score<<'\n';
+        }
 
-void changeColor(int m_id, int color)
-{
-	dfs(m_id, color);
-}
-
-void retrieveColor(int m_id)
-{
-	printf("%d\n", node[m_id].color);
-}
-
-int calculate(NODE cur, int colorCount[])
-{
-	int tempColorCount[MAX_COLOR + 1] = { 0 };
-	tempColorCount[cur.color] = 1;
-
-	int sum = 0;
-	for (int childId : cur.child)
-	{
-		NODE child = node[childId];
-
-		int childColorCount[MAX_COLOR + 1] = { 0 };
-		int score = calculate(child, childColorCount);
-
-		for (int i = 1; i <= MAX_COLOR; i++) tempColorCount[i] += childColorCount[i];
-
-		sum += score;
-	}
-
-	int count = 0;
-	for (int i = 1; i <= 5; i++) count += !!tempColorCount[i];
-
-	sum += (count * count);
-
-	for (int i = 1; i <= MAX_COLOR; i++) colorCount[i] += tempColorCount[i];
-
-	return sum;
-}
-
-int getScore()
-{
-	int score = 0;
-	int colorCount[MAX_COLOR + 1] = { 0 };
-	for (int i = 1; i <= 100000; i++)
-		if (checkRoot[i] == PARENT)
-			score += calculate(node[i], colorCount);
-
-	return score;
-}
-
-void retrieveScore()
-{
-	int score = getScore();
-
-	printf("%d\n", score);
-}
-
-int main()
-{
-	int Q;
-
-	scanf("%d", &Q);
-
-	for (int q = 0; q < Q; q++)
-	{
-		int COMMAND;
-
-		scanf("%d", &COMMAND);
-
-		if (COMMAND == ADD_NODE)
-		{
-			int m_id, p_id, color, max_depth;
-			scanf("%d %d %d %d", &m_id, &p_id, &color, &max_depth);
-			addNode(m_id, p_id, color, max_depth);
-		}
-		else if (COMMAND == CHANGE_COLOR)
-		{
-			int m_id, color;
-			scanf("%d %d", &m_id, &color);
-			changeColor(m_id, color);
-		}
-		else if (COMMAND == RETRIEVE_COLOR)
-		{
-			int m_id;
-			scanf("%d", &m_id);
-			retrieveColor(m_id);
-		}
-		else if (COMMAND == RETRIEVE_SCORE)
-		{
-			retrieveScore();
-		}
-	}
-
-	return 0;
+    }
+    return 0;
 }
