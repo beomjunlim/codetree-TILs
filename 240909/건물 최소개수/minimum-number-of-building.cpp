45341ce51df7c80e3aed546412fcf794ca711331
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-09-09 22:51:48 +0900

    Title: 건물 최소개수, Time: 135ms, Memory: 0MB, Status: Passed - CodeTree

--- 240909/건물 최소개수/minimum-number-of-building.cpp
+++ 240909/건물 최소개수/minimum-number-of-building.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
+#include <algorithm>
+#include <cstring>
 using namespace std;
 
 int X[50001];
 int Y[50001];
+bool height[100001];
 
 int main() {
     int n;
     cin>>n;
 
-    int ans = 0;
-    int min_height = 0;
     for(int i=0; i<n; i++){
         cin>>X[i]>>Y[i];
     }
 
-    min_height = Y[0];
+    int ans = 0;
 
-    for(int i=1; i<n; i++){
-        if(Y[i]>min_height&&Y[i]!=Y[i-1])
-            ans++;
-        
-        if(Y[i]<min_height){
-            ans++;
-            if(Y[i]!=0)
-                min_height = Y[i];
-            
-            if(Y[i]==0)
-                min_height = Y[i+1];
+    for(int i=0; i<n; i++){
+        if(Y[i]==0){
+            memset(height, false, sizeof(height));
+        }
+        else{
+            if(!height[Y[i]]){
+                height[Y[i]] = true;
+                ans++;
+            }
+            for(int j=Y[i]+1; j<=100000; j++){
+                    height[j] = false;
+            }
         }
     }
-
-    ans++;
+    
     cout<<ans;
     return 0;
 }
