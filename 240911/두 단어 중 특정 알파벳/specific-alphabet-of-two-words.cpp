ea5f9b32d4cdda92bff221797ee76ea8628702bb
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-09-11 11:20:56 +0900

    Title: 두 단어 중 특정 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240911/두 단어 중 특정 알파벳/specific-alphabet-of-two-words.cpp
+++ 240911/두 단어 중 특정 알파벳/specific-alphabet-of-two-words.cpp
@@ -4,40 +4,28 @@
 using namespace std;
 
 int n;
-string A[101][2];
 int ans[26];
-string arr[101];
 
-void backtracking(int idx,int cnt){
-    if(cnt==n){
-        int temp[26] = {0};
-        for(int i=0; i<n; i++){
-            string str = arr[i];
-            for(int j=0; j<str.length(); j++){
-                int num = str[j] - 'a';
-                temp[num]++;
-            }
-        }
-
-        for(int i=0; i<26; i++){
-            ans[i] = max(ans[i], temp[i]);
-        }
-        return;
-    }
-
-    arr[idx] = A[idx][0];
-    backtracking(idx+1, cnt+1);
-    arr[idx] = A[idx][1];
-    backtracking(idx+1, cnt+1);
-}
 int main() {
     cin>>n;
 
     for(int i=0; i<n; i++){
-        cin>>A[i][0]>>A[i][1];
-    }
+        string a,b;
+        cin>>a>>b;
 
-    backtracking(0,0);
+        vector<int> freqA(26,0), freqB(26,0);
+
+        for(char c : a){
+            freqA[c - 'a']++;
+        } 
+        for(char c : b){
+            freqB[c - 'a']++;
+        }
+
+        for(int j=0; j<26; j++){
+            ans[j] += max(freqA[j],freqB[j]);
+        }
+    }
 
     for(int i=0; i<26; i++){
         cout<<ans[i]<<'\n';
