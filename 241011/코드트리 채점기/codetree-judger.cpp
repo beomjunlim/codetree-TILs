637d72b42648da92380744fa2e99d17dcd1cae3e
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-10-11 11:42:31 +0900

    Title: 코드트리 채점기, Time: 965ms, Memory: 3MB, Status: Passed - CodeTree

--- 241011/코드트리 채점기/codetree-judger.cpp
+++ 241011/코드트리 채점기/codetree-judger.cpp
@@ -3,14 +3,14 @@
 #include <algorithm>
 #include <queue>
 #include <set>
-#include <map>
+#include <unordered_map>
 #include <cstring>
 #include <cmath>
 using namespace std;
 
 struct Wait {
-    int sequence, time;
-    string url;
+    int sequence, time, Idx;
+    string ID;
 
     bool operator<(const Wait& a) const {
         if (sequence != a.sequence)
@@ -19,12 +19,16 @@ struct Wait {
     }
 };
 
-priority_queue<int> grade;
-set<string> URL;
-priority_queue<Wait> waitting;
-map<string, pair<int, int>> progress; // 채점 시작, 채점기
-map<string, pair<int, int>> End; // 채점 시작 채점 끝
-map<int, pair<string, int>> Pgrade; // 채점 중인거 채점기 도메인 시작 시간
+int DomainIdx = 0;
+int WaittingNum = 0;
+unordered_map<string, int> Domain; // Domain 인덱스
+priority_queue<int> grade; // 채점기
+set<string> URL; // 대기 큐 URL 중복 검사
+priority_queue<Wait>FirstDomain[301]; // DomainIdx 로 가장 빠른 거 비교
+
+unordered_map<string, pair<int, int>> progress; // 채점 시작, 채점기
+unordered_map<string, pair<int, int>> End; // 채점 시작 채점 끝
+unordered_map<int, pair<string, int>> Pgrade; // 채점 중인거 채점기 도메인 시작 시간
 
 int main() {
     int Q;
@@ -42,8 +46,12 @@ int main() {
 
             for (int i = 1; i <= n; i++)
                 grade.push(-i);
-            waitting.push({ 1,0,str });
+
+            Domain[str.substr(0, str.find("/"))] = DomainIdx;
+            FirstDomain[DomainIdx].push({ 1,0,DomainIdx,str.substr(str.find("/")) });
             URL.insert(str);
+            DomainIdx++;
+            WaittingNum++;
         }
         else if (cmd == 200) {
             int t, p;
@@ -53,39 +61,67 @@ int main() {
             if (URL.find(str) != URL.end()) // 이미 있으면 종료
                 continue;
 
+            string domain = str.substr(0, str.find("/"));
+            string ID = str.substr(str.find("/"));
+
+            if (Domain.find(domain) == Domain.end()) {   
+                Domain[domain] = DomainIdx;
+                DomainIdx++;
+            }
+
+            FirstDomain[Domain[domain]].push({ p,t,Domain[domain],ID }); // 대기 큐 Domain 기준 우선 순위
             URL.insert(str); // 대기 큐 URL 관리
-            waitting.push({ p,t,str }); // 대기 큐에 넣기
+            WaittingNum++;
         }
         else if (cmd == 300) {
             int t;
             cin >> t;
 
-            if(waitting.empty()) // 채점할 게 없다면 종료
+            if (WaittingNum == 0) // 채점할 게 없다면 종료
                 continue;
-
-            string str = waitting.top().url;
-            string Domain = str.substr(0, str.find("/")); // 도메인 추출
-
-            if (progress.find(Domain) != progress.end()) // 해당 도메인 채점 중이면 종료
-                continue;
-
-            if (End.find(Domain) != End.end()) { // 채점 종료된 도메인 너무 일찍 나오면 종료
-                int start = End[Domain].first;
-                int gap = End[Domain].second - start;
-
-                if (t < start + 3 * gap)
-                    continue;
-            }
 
             if (grade.empty()) // 채점할 채점기 없다면 종료
                 continue;
 
-            waitting.pop(); // 대기 큐에서 제거
-            URL.erase(str); // 대기 큐 URL 관리
-            int gradeNum = -grade.top(); // 채점 진행할 채점기
-            grade.pop(); // 해당 채점기 제거
-            progress[Domain] = { t, gradeNum }; // 진행 중으로 넣기
-            Pgrade[gradeNum] = { Domain, t }; // 채점기 진행 
+            priority_queue<Wait> pq;
+            for (auto it : Domain) {
+                if (FirstDomain[it.second].empty())
+                    continue;
+
+                int P = FirstDomain[it.second].top().sequence;
+                int T = FirstDomain[it.second].top().time;
+                int Idx = it.second;
+                string url = it.first;
+
+                if (progress.find(url) != progress.end())
+                    continue;
+
+                if (End.find(url) != End.end()) {
+                    int start = End[url].first;
+                    int gap = End[url].second - start;
+                    if (t < start + 3 * gap)
+                        continue;
+                }
+                pq.push({ P,T,Idx, url});
+            }
+
+            if (pq.empty())
+                continue;
+
+            string domain = pq.top().ID;
+            int idx = pq.top().Idx;
+
+            string url = domain + FirstDomain[idx].top().ID;
+
+            int gradeNum = -grade.top();
+            grade.pop();
+
+            progress[domain] = { t, gradeNum }; // 진행 중
+            Pgrade[gradeNum] = { domain, t };
+
+            URL.erase(url);
+            FirstDomain[idx].pop();
+            WaittingNum--;
         }
         else if (cmd == 400) {
             int t, gradeNum;
@@ -93,16 +129,18 @@ int main() {
             string Domain = Pgrade[gradeNum].first; // 해당 채점기가 채점 중인 도메인
             int start = Pgrade[gradeNum].second; // 채점 시작 시간
 
+            if(progress.find(Domain)==progress.end())
+                continue;
+                
             progress.erase(Domain); // 진행 중에서 제거
             Pgrade.erase(gradeNum); // 채점기 진행 중에서 제거
             grade.push(-gradeNum); // 채점기 대기 상태
-
             End[Domain] = { start, t }; // 종료에 넣기 
         }
         else if (cmd == 500) {
             int t;
             cin >> t;
-            cout << waitting.size() << '\n';
+            cout << WaittingNum << '\n';
         }
     }
     return 0;
