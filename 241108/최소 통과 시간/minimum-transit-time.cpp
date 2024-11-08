14d8d71f9d445e883ff69878c71dfd3daa2f3391
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-11-08 10:37:11 +0900

    Title: 최소 통과 시간, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241108/최소 통과 시간/minimum-transit-time.cpp
+++ 241108/최소 통과 시간/minimum-transit-time.cpp
@@ -1,31 +1,41 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 #define MAX 1000000000
 
+int n,m;
 int arr[100001];
 
+bool Possible(long long time){
+    long long cnt = 0;
+    for(int i=0; i<m; i++){
+        cnt += time/arr[i];
+
+        if(cnt>=n)
+            return true;
+    }
+    return false;
+}
+
 int main() {
-    int n,m;
+    
     cin>>n>>m;
 
     for(int i=0; i<m; i++){
         cin>>arr[i];
     }
 
+    sort(arr, arr+m);
+
     long long left = 1;
-    long long right = n*MAX;
+    long long right = (long long)n*arr[m-1];
     long long ans = right;
 
     while(left<=right){
         long long mid = (left + right) / 2;
 
-        int cnt = 0;
-        for(int i=0; i<m; i++){
-            cnt += mid/arr[i];
-        }
-
-        if(cnt>=n){
+        if(Possible(mid)){
             ans = min(ans, mid);
             right = mid - 1;
         }
