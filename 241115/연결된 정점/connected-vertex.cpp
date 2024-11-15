516432b61bf7d14057859088e2f0312434ea68fb
임준범 <104415061+beomjunlim@users.noreply.github.com>
2024-11-15 18:53:40 +0900

    Title: 연결된 정점, Time: 256ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241115/연결된 정점/connected-vertex.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int n,m;
+int parent[100001];
+int count[100001];
+
+int find(int x){
+    if(parent[x]==x)
+        return x;
+    return parent[x] = find(parent[x]);
+}
+
+int main() {
+    cin>>n>>m;
+    
+    for(int i=1; i<=n; i++){
+        parent[i] = i;
+        count[i] = 1;
+    }
+
+
+    for(int i=0; i<m; i++){
+        char a;
+        cin>>a;
+
+        if(a=='x'){
+            int x, y;
+            cin>>x>>y;
+            int X = parent[x];
+            int Y = parent[y];
+
+            parent[X] = Y;
+            count[Y] += count[X];
+        }
+        else{
+            int x;
+            cin>>x;
+            cout<<count[find(x)]<<'\n';
+        }
+    }
+
+    return 0;
+}
